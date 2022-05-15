86a62518591572436bcfa74dfa8cfd23a68eaaec
Roshan-ac <roshanacharya3636@gmail.com>
2022-05-15 10:54:08 -0400

    Lovecalculator version : V1.0.0

---
+++ lovecalculator.sh
@@ -0,0 +1,207 @@
+#!/bin/bash
+ 
+
+# function definition of loader and title
+
+love(){
+echo "
+  OOOOO          OOOOO
+                       OO   OOO      OOO   OO
+                       OO     OOO  OOO     OO
+                        OOO     OOOO     OOO
+      OOOOOO          OO  OOOOOO    OOOOOO  OO          OOOOOO
+   OOOO    OOOOO   OOOO                      OOOO   OOOOO    OOOO
+               OOOOO     VVVVVV      VVVVVV     OOOOO
+                       VVVVVVVVVV  VVVVVVVVVV
+                     VVVVVVVVVVVVVVVVVVVVVVVVVV
+                     VVVVVVVVVVVVVVVVVVVVVVVVVV
+        XXXXXX       VVVVVVVVVVVVVVVVVVVVVVVVVV      XXXXXXX
+     XXXXXXXXXXX      VVVVVVVVVVVVVVVVVVVVVVVV      XXXXXXXXXXX
+    XXXXXXXXXXXXX      VVVVVVVVVVVVVVVVVVVVVV      XXXXXXXXXXXXX
+   XXXXXXXXXXXXXXXX     VVVVVVVVVVVVVVVVVVVV     XXXXXXXXXXXXXXXX
+   XXXXXXXXXXXXXXXX      VVVVVVVVVVVVVVVVVV      XXXXXXXXXXXXXXXX
+   XXXXXXXXXXXXXXXX     XXVVVVVVVVVVVVVVVVXX     XXXXXXXXXXXXXXXX
+    XXXXXXXXXXXXXXXX    XXXVVVVVVVVVVVVVVXXX    XXXXXXXXXXXXXXXX
+       XXXXXXXXXXX     XXXX VVVVVVVVVVVV XXXX     XXXXXXXXXXX
+           XXXXXXX    XXXX   VVVVVVVVVV   XXXX    XXXXXXX
+    XXXXXX  XXXXXXXXXXXXXX    VVVVVVVV    XXXXXXXXXXXXXX  XXXXXX
+  XXXXXXXXXXXXXXXXXXXXXX       VVVVVV       XXXXXXXXXXXXXXXXXXXXXX
+ XXXXXXXXXXXXXXXXXXXX           VVVV           XXXXXXXXXXXXXXXXXXXX
+XX XXXXX XXXXXXXXXXXX            VV            XXXXXXXXXXXX XXXXX XX
+X  X XX  XXXXXXXXXXXX                          XXXXXXXXXXXX  XX X  X
+     X  XXXXXXXXXXXX                            XXXXXXXXXXXX  X
+       XXXXXXXXXXXXX                            XXXXXXXXXXXXX
+       XXXXXXXXXXXXXX                          XXXXXXXXXXXXXX
+       XXXXXXXXXXXXXXX                        XXXXXXXXXXXXXXX
+        XXXXXXXXXXXXXXX                      XXXXXXXXXXXXXXX
+       XXXXXXX  XXXXXXXX                    XXXXXXXX  XXXXXXX
+      XXXXXXX     XXXXXXX                  XXXXXXX     XXXXXXX
+ XXXXXXXXXXX        XXXXXX                XXXXXX        XXXXXXXXXXX
+ XXXXXXXXX           XXXXX                XXXXX           XXXXXXXXX
+ XXXX                 XXXXX              XXXXX                 XXXX
+  XXX                  XXXXX            XXXXX                  XXX
+                        XXXX            XXXX
+ 
+ "
+}
+
+titleBar(){
+echo "	
+    ██▓     ▒█████   ██▒   █▓▓█████        ▄████▄   ▄▄▄       ██▓     ▄████▄   █    ██  ██▓     ▄▄▄      ▄▄▄█████▓ ▒█████   ██▀███        
+   ▓██▒    ▒██▒  ██▒▓██░   █▒▓█   ▀       ▒██▀ ▀█  ▒████▄    ▓██▒    ▒██▀ ▀█   ██  ▓██▒▓██▒    ▒████▄    ▓  ██▒ ▓▒▒██▒  ██▒▓██ ▒ ██▒      
+   ▒██░    ▒██░  ██▒ ▓██  █▒░▒███         ▒▓█    ▄ ▒██  ▀█▄  ▒██░    ▒▓█    ▄ ▓██  ▒██░▒██░    ▒██  ▀█▄  ▒ ▓██░ ▒░▒██░  ██▒▓██ ░▄█ ▒      
+   ▒██░    ▒██   ██░  ▒██ █░░▒▓█  ▄       ▒▓▓▄ ▄██▒░██▄▄▄▄██ ▒██░    ▒▓▓▄ ▄██▒▓▓█  ░██░▒██░    ░██▄▄▄▄██ ░ ▓██▓ ░ ▒██   ██░▒██▀▀█▄        
+   ░██████▒░ ████▓▒░   ▒▀█░  ░▒████▒      ▒ ▓███▀ ░ ▓█   ▓██▒░██████▒▒ ▓███▀ ░▒▒█████▓ ░██████▒ ▓█   ▓██▒  ▒██▒ ░ ░ ████▓▒░░██▓ ▒██▒      
+   ░ ▒░▓  ░░ ▒░▒░▒░    ░ ▐░  ░░ ▒░ ░      ░ ░▒ ▒  ░ ▒▒   ▓▒█░░ ▒░▓  ░░ ░▒ ▒  ░░▒▓▒ ▒ ▒ ░ ▒░▓  ░ ▒▒   ▓▒█░  ▒ ░░   ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░      
+   ░ ░ ▒  ░  ░ ▒ ▒░    ░ ░░   ░ ░  ░        ░  ▒     ▒   ▒▒ ░░ ░ ▒  ░  ░  ▒   ░░▒░ ░ ░ ░ ░ ▒  ░  ▒   ▒▒ ░    ░      ░ ▒ ▒░   ░▒ ░ ▒░      
+     ░ ░   ░ ░ ░ ▒       ░░     ░         ░          ░   ▒     ░ ░   ░         ░░░ ░ ░   ░ ░     ░   ▒     ░      ░ ░ ░ ▒    ░░   ░       
+       ░  ░    ░ ░        ░     ░  ░      ░ ░            ░  ░    ░  ░░ ░         ░         ░  ░      ░  ░             ░ ░     ░           
+                         ░                ░                          ░                                                                    
+"
+}
+progreSh() {
+    LR='\033[1;31m'
+    LG='\033[1;32m'
+    LY='\033[1;33m'
+    LC='\033[1;36m'
+    LW='\033[1;37m'
+    NC='\033[0m'
+    if [ "${1}" = "0" ]; then TME=$(date +"%s"); fi
+    SEC=`printf "%04d\n" $(($(date +"%s")-${TME}))`; SEC="$SEC sec"
+    PRC=`printf "%.0f" ${1}`
+    SHW=`printf "%3d\n" ${PRC}`
+    LNE=`printf "%.0f" $((${PRC}/2))`
+    LRR=`printf "%.0f" $((${PRC}/2-12))`; if [ ${LRR} -le 0 ]; then LRR=0; fi;
+    LYY=`printf "%.0f" $((${PRC}/2-24))`; if [ ${LYY} -le 0 ]; then LYY=0; fi;
+    LCC=`printf "%.0f" $((${PRC}/2-36))`; if [ ${LCC} -le 0 ]; then LCC=0; fi;
+    LGG=`printf "%.0f" $((${PRC}/2-48))`; if [ ${LGG} -le 0 ]; then LGG=0; fi;
+    LRR_=""
+    LYY_=""
+    LCC_=""
+    LGG_=""
+    for ((i=1;i<=13;i++))
+    do
+    	DOTS=""; for ((ii=${i};ii<13;ii++)); do DOTS="${DOTS}."; done
+    	if [ ${i} -le ${LNE} ]; then LRR_="${LRR_}#"; else LRR_="${LRR_}."; fi
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${DOTS}${LY}............${LC}............${LG}............ ${SHW}%${NC}\r"
+    	if [ ${LNE} -ge 1 ]; then sleep .05; fi
+    done
+    for ((i=14;i<=25;i++))
+    do
+    	DOTS=""; for ((ii=${i};ii<25;ii++)); do DOTS="${DOTS}."; done
+    	if [ ${i} -le ${LNE} ]; then LYY_="${LYY_}#"; else LYY_="${LYY_}."; fi
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${DOTS}${LC}............${LG}............ ${SHW}%${NC}\r"
+    	if [ ${LNE} -ge 14 ]; then sleep .05; fi
+    done
+    for ((i=26;i<=37;i++))
+    do
+    	DOTS=""; for ((ii=${i};ii<37;ii++)); do DOTS="${DOTS}."; done
+    	if [ ${i} -le ${LNE} ]; then LCC_="${LCC_}#"; else LCC_="${LCC_}."; fi
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${DOTS}${LG}............ ${SHW}%${NC}\r"
+    	if [ ${LNE} -ge 26 ]; then sleep .05; fi
+    done
+    for ((i=38;i<=49;i++))
+    do
+    	DOTS=""; for ((ii=${i};ii<49;ii++)); do DOTS="${DOTS}."; done
+    	if [ ${i} -le ${LNE} ]; then LGG_="${LGG_}#"; else LGG_="${LGG_}."; fi
+    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${LG}${LGG_}${DOTS} ${SHW}%${NC}\r"
+    	if [ ${LNE} -ge 38 ]; then sleep .05; fi
+    done
+}
+
+
+
+
+
+
+
+
+
+
+read -p 'Did yo have love one. (y/n) : ' input
+clear
+
+if [[ $input == "y" ]]; then
+titleBar
+printf "\n"
+echo "Please be patience while initializing"
+printf "\n\n"
+progreSh 0
+progreSh 10
+progreSh 20
+progreSh 50
+progreSh 60
+progreSh 70
+progreSh 90
+progreSh 100
+printf "\n\n"
+clear
+titleBar
+printf "\n"
+read -p 'Enter you Full Name. (y/n) : ' yourname
+printf "\n"
+progreSh 10
+progreSh 100
+clear
+titleBar
+printf "\n"
+echo "Thanks $yourname"
+printf "\n"
+read -p 'Enter you loveone Name. (y/n) : ' lovername
+printf "\n"
+progreSh 10
+progreSh 100
+clear
+titleBar
+printf "\n\n"
+echo "Are you sure you lovername is $lovername"
+printf "\n\n"
+echo "************* < $yourname ♥ $lovername 3 *****************"
+printf "\n\n"
+echo "Now calculating love in % , please wait........"       
+printf "\n\n\n"
+clear 
+titleBar
+love
+printf "\n\n\n"
+echo "$(( RANDOM % 100 ))% , $yourname and $lovername have great possibility to successfull lover in near future" 
+
+
+else
+	echo "Sorry, this game is not for you"
+
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
